e266d882eadf2a0f6486ecb8af1bbf1c4a894656
Brad P <brad@ad-astra.video>
2024-12-13 20:52:48 -0600

    optimize code to drop image annotations if just want data and add compile to language model

--- nodes.py
+++ nodes.py
@@ -17,12 +17,9 @@ import time
 import json
 import hashlib
 
-
 #workaround for unnecessary flash_attn requirement
 from unittest.mock import patch
 from transformers.dynamic_module_utils import get_imports
-
-
 
 def fixed_get_imports(filename: str | os.PathLike) -> list[str]:
     try:
@@ -67,6 +64,7 @@ class DownloadAndLoadFlorence2Model:
                     {
                     "default": 'sdpa'
                     }),
+            "compile": ("BOOLEAN", {"default": False}),
             },
             "optional": {
                 "lora": ("PEFTLORA",),
@@ -78,7 +76,7 @@ class DownloadAndLoadFlorence2Model:
     FUNCTION = "loadmodel"
     CATEGORY = "Florence2"
 
-    def loadmodel(self, model, precision, attention, lora=None):
+    def loadmodel(self, model, precision, attention, compile, lora=None):
         device = mm.get_torch_device()
         offload_device = mm.unet_offload_device()
         dtype = {"bf16": torch.bfloat16, "fp16": torch.float16, "fp32": torch.float32}[precision]
@@ -95,9 +93,18 @@ class DownloadAndLoadFlorence2Model:
             
         print(f"using {attention} for attention")
         with patch("transformers.dynamic_module_utils.get_imports", fixed_get_imports): #workaround for unnecessary flash_attn requirement
-            model = AutoModelForCausalLM.from_pretrained(model_path, attn_implementation=attention, device_map=device, torch_dtype=dtype,trust_remote_code=True)
+            model = AutoModelForCausalLM.from_pretrained(model_path, attn_implementation=attention, torch_dtype=dtype,trust_remote_code=True).to(device)
+        if compile:
+            model.language_model.forward = torch.compile(model.language_model.forward)
+            model.generate(input_ids=model.dummy_inputs["input_ids"].to(device))
+            
         processor = AutoProcessor.from_pretrained(model_path, trust_remote_code=True)
-        fast_img_proc = AutoImageProcessor.from_pretrained("facebook/detr-resnet-50", size=(768,768), crop_size=(768,768), use_fast=True)
+        fast_img_proc = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k", 
+                                                            size=processor.image_processor.size, 
+                                                            image_mean=processor.image_processor.image_mean, 
+                                                            image_std=processor.image_processor.image_std,
+                                                            image_seq_length=processor.image_processor.image_seq_length,
+                                                            use_fast=True)
         processor.image_processor = fast_img_proc
         
         if lora is not None:
@@ -147,13 +154,14 @@ class Florence2ModelLoader:
     @classmethod
     def INPUT_TYPES(s):
         return {"required": {
-            "model": ([item.name for item in Path(folder_paths.models_dir, "LLM").iterdir() if item.is_dir()], {"tooltip": "models are expected to be in Comfyui/models/LLM folder"}),
+            "model": ([item.name for item in Path(folder_paths.models_dir, "LLM").iterdir() if item.is_dir()], {"tooltip": "models are expected to be in Comfyui/models folder"}),
             "precision": (['fp16','bf16','fp32'],),
             "attention": (
                     [ 'flash_attention_2', 'sdpa', 'eager'],
                     {
                     "default": 'sdpa'
                     }),
+            "compile": ("BOOLEAN", {"default": False}),
             },
             "optional": {
                 "lora": ("PEFTLORA",),
@@ -165,16 +173,25 @@ class Florence2ModelLoader:
     FUNCTION = "loadmodel"
     CATEGORY = "Florence2"
 
-    def loadmodel(self, model, precision, attention, lora=None):
+    def loadmodel(self, model, precision, attention, compile, lora=None):
         device = mm.get_torch_device()
         dtype = {"bf16": torch.bfloat16, "fp16": torch.float16, "fp32": torch.float32}[precision]
-        model_path = Path(folder_paths.models_dir, "LLM", model)
+        model_path = Path(folder_paths.models_dir, model)
         print(f"Loading model from {model_path}")
         print(f"using {attention} for attention")
         with patch("transformers.dynamic_module_utils.get_imports", fixed_get_imports): #workaround for unnecessary flash_attn requirement
-            model = AutoModelForCausalLM.from_pretrained(model_path, attn_implementation=attention, device_map=device, torch_dtype=dtype, trust_remote_code=True)
+            model = AutoModelForCausalLM.from_pretrained(model_path, attn_implementation=attention, torch_dtype=dtype,trust_remote_code=True).to(device)
+        if compile:
+            model.language_model.forward = torch.compile(model.language_model.forward)
+            model.generate(input_ids=model.dummy_inputs["input_ids"].to(device))
+            
         processor = AutoProcessor.from_pretrained(model_path, trust_remote_code=True)
-        fast_img_proc = RTDetrImageProcessorFast.from_pretrained("PekingU/rtdetr_r50vd", size=model_img_size, crop_size=model_crop_size)
+        fast_img_proc = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k", 
+                                                            size=processor.image_processor.size, 
+                                                            image_mean=processor.image_processor.image_mean, 
+                                                            image_std=processor.image_processor.image_std,
+                                                            image_seq_length=processor.image_processor.image_seq_length,
+                                                            use_fast=True)
         processor.image_processor = fast_img_proc
         
         if lora is not None:
@@ -214,7 +231,7 @@ class Florence2Run:
             'ocr': '<OCR>',
             'ocr_with_region': '<OCR_WITH_REGION>',
         }
-        self.uses_text_input = ["referring_expression_segmentation", "caption_to_phrase_grounding", "docvqa", "open_vocabulary_detection"]
+        self.uses_text_input = ["referring_expression_segmentation", "caption_to_phrase_grounding", "open_vocabulary_detection"]
         self.text_responses = ["caption", "ocr", "detail_caption", "more_detailed_caption", "region_to_category", "region_to_description", "region_to_ocr"]
         self.includes_bbox = ["region_caption", "dense_region_caption", "caption_to_phrase_grounding", "open_vocabulary_detection", "ocr_with_region"]
         self.includes_polygons = ["referring_expression_segmentation", "region_to_segmentation"]
@@ -239,7 +256,7 @@ class Florence2Run:
         }
         #load font to use
         try:
-            self.font = ImageFont.load_default().font_variant(size=24)
+            self.font = ImageFont.load_default().font_variant(size=12)
         except:
             self.font = ImageFont.load_default()
         
@@ -281,10 +298,11 @@ class Florence2Run:
             },
             "optional": {
                 "output_mask_select": ("STRING", {"default": ""}),
-                "fill_mask": ("BOOLEAN", {"default": False}),
                 "max_new_tokens": ("INT", {"default": 1024, "min": 1, "max": 4096}),
                 "num_beams": ("INT", {"default": 1, "min": 1, "max": 64}),
                 "do_sample": ("BOOLEAN", {"default": False}),
+                "annotate_image": ("BOOLEAN", {"default": False}),
+                "fill_mask": ("BOOLEAN", {"default": False}),
             }
         }
     
@@ -306,7 +324,7 @@ class Florence2Run:
                 **kwargs,
             )
 
-    def process_polygons_and_labels(self, image_pil, polygons, labels, fill_mask=False, annotation_color=(255, 0, 0)):
+    def process_polygons_and_labels(self, image_pil, polygons, labels, annotate_image=False, fill_mask=False, annotation_color=(255, 0, 0)):
         # Create a new black image
         mask_image = Image.new('RGB', (W, H), 'black')
         mask_draw = ImageDraw.Draw(mask_image)
@@ -324,19 +342,21 @@ class Florence2Run:
                 _polygon = _polygon.reshape(-1).tolist()
                 
                 # Draw the polygon
-                if fill_mask:
-                    overlay = Image.new('RGBA', image_pil.size, (255, 255, 255, 0))
-                    image_pil = image_pil.convert('RGBA')
-                    draw = ImageDraw.Draw(overlay)
-                    color_with_opacity = ImageColor.getrgb(annotation_color) + (180,)
-                    draw.polygon(_polygon, outline=annotation_color, fill=color_with_opacity, width=3)
-                    image_pil = Image.alpha_composite(image_pil, overlay)
-                else:
-                    draw = ImageDraw.Draw(image_pil)
-                    draw.polygon(_polygon, outline=annotation_color, width=3)
+                if annotate_image:
+                    if fill_mask:
+                        overlay = Image.new('RGBA', image_pil.size, (255, 255, 255, 0))
+                        image_pil = image_pil.convert('RGBA')
+                        draw = ImageDraw.Draw(overlay)
+                        color_with_opacity = ImageColor.getrgb(annotation_color) + (180,)
+                        draw.polygon(_polygon, outline=annotation_color, fill=color_with_opacity, width=3)
+                        image_pil = Image.alpha_composite(image_pil, overlay)
+                    else:
+                        draw = ImageDraw.Draw(image_pil)
+                        draw.polygon(_polygon, outline=annotation_color, width=3)
 
                 #draw mask
-                mask_draw.polygon(_polygon, outline="white", fill="white")
+                if fill_mask:
+                    mask_draw.polygon(_polygon, outline="white", fill="white")
                 
         annotated_image_tensor = F.to_tensor(image_pil)
         annotated_image_tensor = annotated_image_tensor[:3, :, :].unsqueeze(0).permute(0, 2, 3, 1).cpu().float() 
@@ -349,7 +369,7 @@ class Florence2Run:
         
         return annotated_image_tensor, mask_tensor
                 
-    def process_bboxes_and_labels(self, image_pil, boxes, labels, mask_indexes, fill_mask=False, annotation_color=(255, 0, 0), exclude_labels=False):
+    def process_bboxes_and_labels(self, image_pil, boxes, labels, mask_indexes, annotate_image=False, fill_mask=False, annotation_color=(255, 0, 0), exclude_labels=False):
         W, H = image_pil.size  # Image dimensions
         
         # Initialize mask_layer only if needed
@@ -357,44 +377,45 @@ class Florence2Run:
             mask_layer = Image.new('RGB', image_pil.size, (0, 0, 0))  # Blank mask image
             mask_draw = ImageDraw.Draw(mask_layer)
         
-        # Convert image to RGB format if needed
-        draw = ImageDraw.Draw(image_pil)
-        
-        # Draw bounding boxes and labels
-        for index, (box, label) in enumerate(zip(boxes, labels)):
-            # Modify the label to include the index
-            indexed_label = f"{index}.{label}" if not exclude_labels else f"{index}"
+        if annotate_image:
+            draw = ImageDraw.Draw(image_pil)
             
-            # Draw bounding box
-            # most tasks return bboxes [x0,y0,x1,y1]
-            # ocr_with_region returns quad_boxes [x0,y0 ... x3,y3]
-            x0 = box[0]
-            y0 = box[1] if len(box) == 4 else box[1]
-            x1 = box[2] if len(box) == 4 else max(box[0], box[2]+5) #small buffer to make sure is larger
-            y1 = box[3] if len(box) == 4 else max(box[1], box[7]+5) #small buffer to make sure is larger
-            
-            draw.rectangle([x0, y0, x1, y1], outline=annotation_color, width=2)
-            
-            # Optionally add label
-            text_width = len(label) * 6  # Adjust multiplier based on your font size
-            text_height = 12  # Adjust based on your font size
-            
-            # Initial text position
-            text_x = x0
-            text_y = y0 - text_height  # Position text above the top-left of the bbox
-            
-            # Adjust text_x if text is going off the left or right edge
-            if text_x < 0:
-                text_x = 0
-            elif text_x + text_width > W:
-                text_x = W - text_width
-            
-            # Adjust text_y if text is going off the top edge
-            if text_y < 0:
-                text_y = y1  # Move text below the bottom-left of the bbox if it doesn't overlap with bbox
-            
-            # Add the label text
-            draw.text((text_x, text_y), indexed_label, fill=annotation_color)
+            # Draw bounding boxes and labels
+            for index, (box, label) in enumerate(zip(boxes, labels)):
+                # Modify the label to include the index
+                indexed_label = f"{index}.{label}" if not exclude_labels else f"{index}"
+                
+                # Draw bounding box
+                # most tasks return bboxes [x0,y0,x1,y1]
+                # ocr_with_region returns quad_boxes [x0,y0 ... x3,y3]
+                x0 = box[0]
+                y0 = box[1]
+                x1 = box[2] if len(box) == 4 else max(box[0], box[2]+5) #small buffer to make sure is larger
+                y1 = box[3] if len(box) == 4 else max(box[1], box[7]+5) #small buffer to make sure is larger
+                
+                draw.rectangle([x0, y0, x1, y1], outline=annotation_color, width=2)
+                
+                # Optionally add label
+                text_width = len(label) * 6  # Adjust multiplier based on your font size
+                text_height = 12  # Adjust based on your font size
+                
+                # Initial text position
+                text_x = x0
+                text_y = y0 - text_height - 2 # Position text above the top-left of the bbox
+                
+                # Adjust text_x if text is going off the left or right edge
+                if text_x < 0:
+                    text_x = 0
+                elif text_x + text_width > W:
+                    text_x = W - text_width
+                
+                # Adjust text_y if text is going off the top edge
+                if text_y < 0:
+                    text_y = y1  # Move text below the bottom-left of the bbox if it doesn't overlap with bbox
+                
+                # Add the label text
+                draw.rectangle([text_x, text_y, x1, max(text_y+1,y0)], outline=annotation_color, width=2, fill=annotation_color)
+                draw.text((text_x, text_y), indexed_label, font=self.font, fill='white')
             
             # Optionally add the mask
             if fill_mask:
@@ -444,7 +465,7 @@ class Florence2Run:
         
         return processing_stats
         
-    def encode(self, image, text_input, florence2_model, mode, task, annotation_color, fill_mask, keep_model_loaded=True, 
+    def encode(self, image, text_input, florence2_model, mode, task, annotation_color, fill_mask=False, annotate_image=False, keep_model_loaded=True, 
             num_beams=1, max_new_tokens=1024, do_sample=True, output_mask_select=""):
         
         if mode == "on task change" and self.skip_encode(text_input, task, annotation_color, output_mask_select):
@@ -475,7 +496,6 @@ class Florence2Run:
         processing_stats = {}
         for img in image:
             start = time.time()
-            
             image_pil = F.to_pil_image(img)
             inputs = processor(text=prompt, images=image_pil, return_tensors="pt").to(dtype).to(self.device)
             self.track_processing_stats(processing_stats, "preprocess", int((time.time()-start)*1000))
@@ -512,13 +532,22 @@ class Florence2Run:
             W, H = image_pil.size
             parsed_answer = processor.post_process_generation(results, task=task_prompt, image_size=(W, H))
             self.track_processing_stats(processing_stats, "postprocess", int((time.time()-start)*1000))
-            
+            #print(results)
+            #print(parsed_answer)
             if task in self.includes_polygons:
                 predictions = parsed_answer[task_prompt]
                 
-                out_tensor, out_mask = self.process_polygons_and_labels(image_pil, predications['polygons'], predictions['labels'], fill_mask, self.colors_rgb[annotation_color])
+                if annotate_image or fill_mask:
+                    out_tensor, out_mask = self.process_polygons_and_labels(image_pil, 
+                                                                            predications['polygons'], 
+                                                                            predictions['labels'], 
+                                                                            annotate_image, 
+                                                                            fill_mask, 
+                                                                            self.colors_rgb[annotation_color],
+                                                                           )
                 
-                out.append(out_tensor)
+                if annotate_image:
+                    out.append(out_tensor)
                 if fill_mask:
                     out_masks.append(out_mask)
                     
@@ -536,10 +565,21 @@ class Florence2Run:
                 else:
                     mask_indexes = [str(i) for i in range(len(bboxes))]
                 
-                out_tensor, out_mask = self.process_bboxes_and_labels(image_pil, bboxes, labels, mask_indexes, fill_mask, self.colors_rgb[annotation_color], exclude_labels_annotation)
+                if annotate_image or fill_mask:
+                    out_tensor, out_mask = self.process_bboxes_and_labels(image_pil, 
+                                                                          bboxes, 
+                                                                          labels, 
+                                                                          mask_indexes, 
+                                                                          annotate_image,
+                                                                          fill_mask, 
+                                                                          self.colors_rgb[annotation_color], 
+                                                                          exclude_labels_annotation,
+                                                                         )
                 
-                out.append(out_tensor)
                 out_data.append(bboxes)
+                
+                if annotate_image:
+                    out.append(out_tensor)
                 if fill_mask:
                     out_masks.append(out_mask)
                 
